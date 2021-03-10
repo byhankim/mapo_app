45018e4a9b47642d9e6442cefb2c657b678b4bb4
hankim <freshpodo@icloud.com>
2021-03-11 00:46:30 +0900

    Add lotto generator code

---
+++ KotlinPrac00/src/kr/co/kotlin/HelloKotlin.kt
@@ -0,0 +1,25 @@
+package kr.co.kotlin
+
+import java.util.*
+import kotlin.random.Random
+
+fun main(args: Array<String>) {
+    var count = Scanner(System.`in`).nextInt()
+    if (count < 1 || count > 5) {
+        println("최소 게임 횟수는 1회, 최대 횟수는 5회 입니다. 3회로 강제 조정합니다.")
+        count = 3
+    }
+
+//    val totalGames = mutableListOf<Set<Int>>()
+    for (i in 1..count) {
+        val lottery = mutableSetOf<Int>()
+        for (j in 1..7) {
+            lottery.add(Random.nextInt(45)+1)
+        }
+        while (lottery.size < 7) {
+            lottery.add(Random.nextInt(45)+1)
+        }
+        println("$i 번째 게임: $lottery")
+//        totalGames.add(lottery)
+    }
+}
