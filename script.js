8f5db40dabb8f5d4f90995f01a38f3aa36842ff2
hankim <42214405+byhankim@users.noreply.github.com>
2022-04-25 10:13:57 +0900

    Add: mapo.go.kr crawl w/ selenium

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const MAPOGU_OFFICE_LNG = 126.90198620741052;
 let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
 let options = {
   //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+  center: new kakao.maps.LatLng(MAPOGU_OFFICE_LAT, MAPOGU_OFFICE_LNG), //지도의 중심좌표.
   level: 3, //지도의 레벨(확대, 축소 정도)
 };
 
@@ -21,4 +21,50 @@ function smoothScrollTo(lat, lng) {
   map.panTo(newLocation);
 }
 
-// 마구로 위치 설정하기
+// places 에 리스트 추가
+// JSON 파싱해서 리스트로 넣어놓기
+let hotPlaces = [
+  {
+    id: 0,
+    name: "백미향1",
+    type: "중식",
+    mainMenu: "짜장면",
+    addrs: "서울특별시 마포구 성지길 27 디엠아이빌딩 1층",
+    telephone: "312-8253",
+    website: "http://place.map.daum.net/15678895",
+  },
+  {
+    id: 1,
+    name: "백미향2",
+    type: "중식",
+    mainMenu: "짜장면",
+    addrs: "서울특별시 마포구 성지길 27 디엠아이빌딩 1층",
+    telephone: "312-8253",
+    website: "http://place.map.daum.net/15678895",
+  },
+  {
+    id: 2,
+    name: "백미향3",
+    type: "중식",
+    mainMenu: "짜장면",
+    addrs: "서울특별시 마포구 성지길 27 디엠아이빌딩 1층",
+    telephone: "312-8253",
+    website: "http://place.map.daum.net/15678895",
+  },
+];
+let places = document.querySelector(".places");
+hotPlaces.forEach((obj) => {
+  let div = document.createElement("div");
+  div.innerHTML = `${obj.id} - ${obj.name} - ${obj.type} - ${obj.addrs}<bt/>`;
+  places.appendChild(div);
+});
+
+// let food = {
+//   id: 0,
+//   name: "백미향",
+//   type: "중식",
+//   mainMenu: "짜장면",
+//   addrs: "서울특별시 마포구 성지길 27 디엠아이빌딩 1층",
+//   telephone: "312-8253",
+//   website: "http://place.map.daum.net/15678895",
+// };
